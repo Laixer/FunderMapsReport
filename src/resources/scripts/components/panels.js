17214b04632ba328f8e2a6f418aabfe6f0507120
Kris <kris@lemone.com>
2024-02-06 16:22:00 +0100

    ✨ Add example of a 2nd level panel

--- src/resources/scripts/components/panels.js
+++ src/resources/scripts/components/panels.js
@@ -1,11 +1,19 @@
 export default () => ({
   leftPanelOpen: false,
+  leftPanelSlide: false,
   rightPanelOpen: false,
+  rightPanelSlide: false,
 
   toggleLeftPanel() {
     this.leftPanelOpen = !this.leftPanelOpen;
   },
   toggleRightPanel() {
     this.rightPanelOpen = !this.rightPanelOpen;
+  },
+  slideLeftPanel() {
+    this.leftPanelSlide = !this.leftPanelSlide;
+  },
+  slideRightPanel() {
+    this.rightPanelSlide = !this.rightPanelSlide;
   },
 });
