8c250f908738ea1543165bb4359f7def89831689
Kris <kris@lemone.com>
2024-02-19 19:15:31 +0100

    ✨ Add a chart to the maps demo

--- src/resources/scripts/app.js
+++ src/resources/scripts/app.js
@@ -1,17 +1,26 @@
 import Alpine from "alpinejs";
 import ui from "@alpinejs/ui";
 import focus from "@alpinejs/focus";
+
+// Import Components
 import panels from "./components/panels";
+import chartLine from "./components/chartLine";
 
-window.Alpine = Alpine;
+document.addEventListener("DOMContentLoaded", () => {
+  window.Alpine = Alpine;
 
-// AlpineJS Components: Headless UI
-// https://alpinejs.dev/components
-Alpine.plugin(ui);
+  // AlpineJS Components: Headless UI
+  // https://alpinejs.dev/components
+  Alpine.plugin(ui);
 
-// AlpineJS PLugin: Collapse for smooth animations
-Alpine.plugin(focus);
+  // AlpineJS PLugin: Collapse for smooth animations
+  Alpine.plugin(focus);
 
-Alpine.data("panels", panels);
+  Alpine.data("panels", panels);
+  Alpine.data("chartLine", chartLine);
 
-Alpine.start();
+  Alpine.start();
+
+  // eslint-disable-next-line no-console
+  console.log("hello from app.js");
+});
