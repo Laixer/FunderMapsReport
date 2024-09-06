4ffa7e78d0848689e29fc3e4ef7b8a598da98814
Wouter van Dam <wouter@journeyworks.nl>
2024-09-06 11:10:01 +0200

    Only include page content

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="page-print-preview | content | space-y-12">
-    <div class="a4">
-      <header class="header--print | relative isolate overflow-hidden rounded-md">
-        <h1>Building not found</h1>
-      </header>
+  <header class="header--print | relative isolate overflow-hidden rounded-md">
+    <h1>Building not found</h1>
+  </header>
 
-      <article class="space-y-5">
-        <section class="space-y-4 text-grey-700">
-          <p>
-            The building reference is unkown.
-          </p>
-        </section>
-      </article>
-    </div>
-  </div>
+  <article class="space-y-5">
+    <section class="space-y-4 text-grey-700">
+      <p>
+        The building reference is unkown.
+      </p>
+    </section>
+  </article>
 </template>
